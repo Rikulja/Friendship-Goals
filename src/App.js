408472f3a09ad7a5cd5df7c8bed6431cb7218b3e
Tanjushka <rikabrutal@gmail.com>
2022-11-18 15:16:49 +0200

    Add initial css styles for app and background

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import { useState } from "react";
 //import components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
+import NavMenu from "./components/NavMenu";
 //import styles
 import "./styles/app.scss";
+//import background
+import aurora from "./images/aurora.jpg";
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <h1>Friendship Goals</h1>
-      <Form
-        inputText={inputText}
-        setInputText={setInputText}
-        todos={todos}
-        setTodos={setTodos}
-      />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <NavMenu />
+      <div
+        className="img-background"
+        style={{ backgroundImage: `url(${aurora})` }}
+      >
+        <Form
+          inputText={inputText}
+          setInputText={setInputText}
+          todos={todos}
+          setTodos={setTodos}
+        />
+        <TodoList todos={todos} setTodos={setTodos} />
+      </div>
     </div>
   );
 }
---
+++ src/components/NavMenu.js
@@ -0,0 +1,9 @@
+const NavMenu = () => {
+  return (
+    <header>
+      <h1>Friendship Goals</h1>
+    </header>
+  );
+};
+
+export default NavMenu;
